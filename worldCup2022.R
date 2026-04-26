81e87953247edd256f83e162e32d69b300da683e
rdj2492 <jones.ryland2492@gmail.com>
2026-04-26 12:10:04 -0400

    Upload World Cup R File
    Upload the R file for the World Cup 2022 data project
    Fixes issue #3

---
+++ worldCup2022.R
@@ -0,0 +1,27 @@
+# 2022 World Cup Data ----
+## Get data from the 2022 World Cup and create plots showing statistics by
+## team vs average team age
+
+# Step 1: Load Packages ----
+## Needed Packages: tidyverse, knitr, rvest, kableExtra, readxl
+library(tidyverse)
+library(knitr)
+library(rvest)
+library(kableExtra)
+library(readxl)
+
+# Step 2: Load Data ----
+## Needed data: World Cup Team Data
+
+worldCupRaw <- read_xlsx(
+  path = "worldCup2022.xlsx"
+  )
+
+View(worldCupRaw)
+# Step 3: short description ----
+
+# Step 4: short description ----
+
+## Step 4A: short description ----
+
+## Step 4B: short description ----
